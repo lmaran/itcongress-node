6faa39652af6da2e740959bb79dac50ae18a4b70
Lucian M <lmaran@users.noreply.github.com>
2016-04-04 10:05:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World3\n');
 }).listen(port);

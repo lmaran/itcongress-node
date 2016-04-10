90fc7676a751c24cf702a482e5db936a503385e5
Lucian Maran <lucian.maran@outlook.com>
2016-04-10 19:39:00 +0300

    updata npm packages

--- server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World'+  ', ' + proces.env.NODE_ENV);
-}).listen(port);
-console.log('started on ' + port + ', ' + proces.env.NODE_ENV);
